485e04f46ffdc1b410b535f66624b3fa15966c10
uzdeveloper1 <65232715+uzdeveloper1@users.noreply.github.com>
2022-05-14 20:19:42 +0500

    some bugs fixed

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (){
-    return redirect()->route('home');
-});
 
 //Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
